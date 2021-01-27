fd627e7c972e82cdd5d1391483c2a584ab26acc4
Elogeek <elodiechristin@gmail.com>
2021-01-27 15:01:31 +0100

    finish

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
 
 // Premiere ligne
+// date du jour
 
+//exo1
+echo date('l j F Y, H:i')."<br>";
 
+//exo 2
+echo $date = date("d.m.Y")."<br>";
 
+//exo 3
+echo $today = date("H:i:s")."<br>";
 
 
 
